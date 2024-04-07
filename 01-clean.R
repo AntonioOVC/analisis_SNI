b3033442568e7fbb1fde4153919cf3dd9ca85148
Antonio Ovalle <antonio.oc95@gmail.com>
2024-04-07 13:05:38 -0600

    Proporcion Dr y Dra pierden SNI

--- 01-clean.R
+++ 01-clean.R
@@ -16,3 +16,21 @@ dir.create(path="results")
 #guardar esta data 
 
 write.csv (x = data_filtrada, file ="results/limpia.csv")
+
+
+#Calcular la proporcion de Doctores y Doctoraas que pierden SNI 
+
+data_resumen <- data_filtrada %>% 
+  filter(cambio == "perdio SNI") %>%
+  group_by(nobilis) %>%
+  summarise (totales = sum(miembros))
+
+
+# Guardamos la data resumen
+write.csv( x = data_resumen, file = "results/resumen.csv" )
+
+
+
+#To-do 
+#antoniooc: calcular el porcentaje de dr y dra que pierden el SNI 
+# como total ingresos/ total perdidas.
